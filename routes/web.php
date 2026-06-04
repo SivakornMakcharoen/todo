86c340f6b218ee502aff98e6596c65638fbd0be6
myteryma <pingillyclubs@gmail.com>
2026-06-04 18:22:14 +0700

    Add registration functionality and views for user registration

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@ Route::redirect('/', '/login');
 Route::middleware('guest')->group(function () {
     Route::get('login', [AuthController::class, 'showLogin'])->name('login');
     Route::post('login', [AuthController::class, 'login']);
+
+    Route::get('register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('register', [AuthController::class, 'register']);
 });
 
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
